5fb1b28f664cef428062e93a78a48165a4203fbc
Godfred <goddysmart2468@gmail.com>
2026-02-14 12:26:25 +0000

    GET and POST Endpoint Created

---
+++ apps/backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// GET Health Check
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
+
+// POST messages
+app.post("/api/v1/messages", (req, res) => {
+  const { message } = req.body;
+  res.status(201).json({
+    received: message,
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
